680f09db9866709a16c7a41f9007ce8da378ff12
Frank Blanning <laserscout@gmail.com>
2021-10-15 09:46:47 +0300

    Set discovery metadata

--- firmware/HID.cpp
+++ firmware/HID.cpp
@@ -226,8 +226,8 @@ void HID::begin(void) {
   Bluefruit.setTxPower(0);
   Bluefruit.autoConnLed(false);
 
-  bleDIS.setManufacturer("TODO");
-  bleDIS.setModel("TODO");
+  bleDIS.setManufacturer("BLNET");
+  bleDIS.setModel("AdvantageBLE");
   
   bleDIS.begin();
   bleHID.begin();
